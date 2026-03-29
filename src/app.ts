7a7f1f65d30ee125756a9640956dcc596bde7ded
Deisy Mariana Acosta Correa <deisymarcosta13@gmail.com>
2026-03-29 20:34:56 +0200

    feat: configuración de entorno, variables .env y conexión a MySQL con puerto dinámico

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,28 @@
 import express from "express";
+import { env } from "./config/env";
+import { db } from "./persistence/mysql.connection";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API funcionando ");
+app.get("/", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT 1");
+    res.json({
+      message: "API funcionando ",
+      db: "Conectada "
+    });
+  } catch (error: any) {
+    console.error(" ERROR DB:", error.message);
+
+    res.status(500).json({
+      message: "Error conectando a DB ",
+      error: error.message
+    });
+  }
 });
 
-const PORT = 4000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(env.PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${env.PORT}`);
 });
---
+++ src/config/env.ts
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import path from "path";
+
+// 🔥 FORZAMOS lectura del .env correctamente
+dotenv.config({
+  path: path.resolve(process.cwd(), ".env")
+});
+
+class EnvConfig {
+  public readonly PORT: number;
+  public readonly DB_HOST: string;
+  public readonly DB_PORT: number;
+  public readonly DB_USER: string;
+  public readonly DB_PASSWORD: string;
+  public readonly DB_NAME: string;
+  public readonly JWT_SECRET: string;
+
+  private static instance: EnvConfig;
+
+  private constructor() {
+    console.log("🔍 Variables cargadas:");
+    console.log("PORT:", process.env.PORT);
+    console.log("DB_USER:", process.env.DB_USER);
+
+    this.PORT = Number(process.env.PORT) || 5000;
+    this.DB_HOST = process.env.DB_HOST || "localhost";
+    this.DB_PORT = Number(process.env.DB_PORT) || 3306;
+    this.DB_USER = process.env.DB_USER || "";
+    this.DB_PASSWORD = process.env.DB_PASSWORD || "";
+    this.DB_NAME = process.env.DB_NAME || "";
+    this.JWT_SECRET = process.env.JWT_SECRET || "";
+  }
+
+  public static getInstance(): EnvConfig {
+    if (!EnvConfig.instance) {
+      EnvConfig.instance = new EnvConfig();
+    }
+    return EnvConfig.instance;
+  }
+}
+
+export const env = EnvConfig.getInstance();
