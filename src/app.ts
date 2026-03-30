d48888bf9daca05352d5638ba6d13e75299537f2
Deisy Mariana Acosta Correa <deisymarcosta13@gmail.com>
2026-03-30 17:20:14 +0200

    fix(app): corregir el orden de montaje de rutas y los puntos finales de la API

--- src/app.ts
+++ src/app.ts
@@ -13,25 +13,24 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 console.log("Documentación disponible en /api-docs");
 
+// Ruta de prueba
 app.get("/", async (req, res) => {
   try {
     const [rows] = await db.query("SELECT 1");
     res.json({
-      message: "API funcionando ",
-      db: "Conectada "
+      message: "API funcionando",
+      db: "Conectada"
     });
   } catch (error: any) {
-    console.error(" ERROR DB:", error.message);
-
     res.status(500).json({
-      message: "Error conectando a DB ",
+      message: "Error conectando a DB",
       error: error.message
     });
   }
 });
 
+app.use("/api", routes);
+
 app.listen(env.PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${env.PORT}`);
 });
-
-app.use("/api", routes);
